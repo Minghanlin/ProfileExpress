25023e7ae78cedeca2aad940a5bb48ffd217229a
Minghan Lin <cd>
2016-08-17 12:03:31 +0800

    change data

--- app/controllers/profile.server.controller.js
+++ app/controllers/profile.server.controller.js
@@ -5,7 +5,7 @@ module.exports = {
           name: 'Lin Minghan',
           email: 'minghanmail@yahoo.com',
           hobbies: 'blah blah blah',
-          description: 'blah blah blah'
+          description: 'boom boom boom boom boom'
         }
       );
   }
